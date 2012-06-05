eb6514eb14104b20188219547384419ffd425b99
Zach Peacock <zach@thoomtech.com>
2012-06-04 21:39:41 -0600

    Initial code commit.

---
+++ Giply.php
@@ -0,0 +1,163 @@
+<?php
+/**
+ * Giply
+ *
+ * @author Zach Peacock <zdp@thoomtech.com>
+ * @copyright Copyright (c) 2012, Thoom Technologies LLC
+ *
+ */
+
+class Giply
+{
+    const LOG_DEBUG = 'DEBUG';
+    const LOG_ERR = 'ERROR';
+    const LOG_INFO = 'INFO';
+
+    /**
+     * A callback function to call after the deploy has finished.
+     *
+     * @var callback
+     */
+    public $post_deploy;
+    
+    
+    private $composer = '/usr/local/bin/composer.phar';
+
+    /**
+     * The name of the file that will be used for logging deployments. Set to
+     * FALSE to disable logging.
+     *
+     * @var string
+     */
+    private $log = 'deployments.log';
+
+    /**
+     * The timestamp format used for logging.
+     *
+     * @link    http://www.php.net/manual/en/function.date.php
+     * @var     string
+     */
+    private $date_format = 'Y-m-d H:i:sP';
+
+    /**
+     * The name of the branch to pull from.
+     *
+     * @var string
+     */
+    private $branch = 'master';
+
+    /**
+     * The name of the remote to pull from.
+     *
+     * @var string
+     */
+    private $remote = 'origin';
+
+    /**
+     * The directory where your website and git repository are located, can be
+     * a relative or absolute path
+     *
+     * @var string
+     */
+    private $directory;
+
+
+    private $exec;
+
+    /**
+     * Sets up defaults.
+     *
+     * @param  string  $directory  Directory where your website is located
+     * @param  array   $data       Information about the deployment
+     */
+    public function __construct($directory, $options = array())
+    {
+        // Determine the directory path
+        $this->directory = realpath($directory) . DIRECTORY_SEPARATOR;
+
+        if (is_readable($this->directory . "giply.json"))
+            $options = array_merge(json_decode(file_get_contents($this->directory . "Giply.json")), $options);
+
+        $available_options = array('log', 'date_format', 'branch', 'remote', 'composer', 'exec');
+
+        foreach ($options as $option => $value) {
+            if (in_array($option, $available_options)) {
+                $this->$option = $value;
+            }
+        }
+
+        $this->log('Attempting deployment...');
+    }
+
+    /**
+     * Writes a message to the log file.
+     *
+     * @param  string  $message  The message to write
+     * @param  string  $type     The type of log message (e.g. INFO, DEBUG, ERROR, etc.)
+     */
+    public function log($message, $type = self::LOG_INFO)
+    {
+        if ($this->log) {
+            // Set the name of the log file
+            $filename = $this->log;
+
+            if (!file_exists($filename)) {
+                // Create the log file
+                file_put_contents($filename, '');
+
+                // Allow anyone to write to log files
+                chmod($filename, 0666);
+            }
+
+            // Write the message into the log file
+            // Format: time --- type: message
+            file_put_contents($filename, date($this->date_format) . ' --- ' . $type . ': ' . $message . PHP_EOL, FILE_APPEND);
+        }
+    }
+
+    /**
+     * Executes the necessary commands to deploy the website.
+     */
+    public function execute()
+    {
+        try {
+            // Make sure we're in the right directory
+            exec("cd  $this->directory", $output);
+            $this->log('Changing working directory... ' . implode(' ', $output));
+
+            // Discard any changes to tracked files since our last deploy
+            exec('git reset --hard HEAD', $output);
+            $this->log('Resetting repository... ' . implode(' ', $output));
+
+            // Update the local repository
+            exec('git pull ' . $this->remote . ' ' . $this->branch, $output);
+            $this->log('Pulling in changes... ' . implode(' ', $output));
+
+            // Secure the .git directory
+            exec('chmod -R og-rx .git', $output);
+            $this->log('Securing .git directory... ');
+
+            if (is_readable($this->directory ."composer.json")){
+                exec("php $this->composer self-update", $output);
+
+                if (!file_exists($this->directory . "composer.lock"))
+                    exec("php $this->composer install", $output);
+                else
+                    exec("php $this->composer update", $output);
+            }
+
+            if ($this->exec){
+                foreach ($this->exec as $exec)
+                    exec($exec, $output);
+            }
+
+            if (is_callable($this->post_deploy))
+                call_user_func($this->post_deploy);
+
+            $this->log('Deployment successful.');
+        } catch (Exception $e) {
+            $this->log($e, self::LOG_ERR);
+        }
+    }
+
+}
--- README.md
+++ README.md
@@ -1,4 +1,36 @@
 Giply
 =====
 
-Giply is git-based deployment server/receiver written in PHP.
+Giply is a git-based deployment server/receiver written in PHP. It's very much early stage, being used on several
+personal/small projects, so the options are pretty rudimentary at the moment.
+
+The library is split into 2 parts. The Giply class that you can use in your own scripts and a simple index.php file
+that can be used to manage a Giply server. Read on to learn more about both!
+
+Class: Giply.php
+----------------
+
+This class does all of the work pulling the latest data from the Git repo. It will look for a __giply.json__ file in the
+project's working directory to overwrite any of the default variables. Additionally, if a __composer.json__ file exists
+in the project's working directory, the class will attempt to run the composer.phar file (it assumes that the the phar is
+located in the _/usr/local/bin_ directory).
+
+Server: index.php
+-----------------
+
+This server script is included since I use the same server to host several small projects. This script allows me to
+have a single domain (i.e. deploy.myserver.com) that I can use to manage POST deployments for all of my projects on the
+server.
+
+The server expects pretty URLs, in the format: __action/projectName/securityHash__. The only action supported at the moment
+is _pull_. The other options: _projectName_ is the name of the working directory (in your /var/www folder). The security
+hash is a simple md5 hash of the string _/var/www/projectName_. Other hashes may be supported in future revisions, and
+it is really only there to add a little bit of simple security.
+
+References
+----------
+
+There are two blog posts that have directly inspired Giply:
+
+ 1. http://seancoates.com/blogs/deploy-on-push-from-github
+ 2. http://brandonsummers.name/blog/2012/02/10/using-bitbucket-for-automated-deployments
